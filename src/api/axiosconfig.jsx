ab9a551273443c1e9045a8358bbcc6eadc5f1b4f
Danial Siddique <danialsiddique2004@gmail.com>
2026-08-23 23:49:13 +0500

    update backend API url

--- src/api/axiosconfig.jsx
+++ src/api/axiosconfig.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:3000/"
+    baseURL:"https://ecommerce-web-backend-8pen.onrender.com/"
 })
 export default instance
